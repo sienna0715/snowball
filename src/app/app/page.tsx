d54b582c32c559756295b6257cdc3cf780edb118
Sion Lee <91577106+sienna0715@users.noreply.github.com>
2025-08-28 14:58:41 +0900

    👷‍♀️ refactor: restructure folders into (app) and (auth) route groups

---
+++ src/app/app/page.tsx
@@ -0,0 +1,33 @@
+import { ChartPieLabel } from "@/components/layout/PieChart"; 
+import { getApplications } from "@/service/applications";
+import { DataTable as ApplicationTable } from "./applications/data-table";
+import { columns as ApplicationColumns } from "./applications/columns"
+import { getCoverletter } from "@/service/coverletter";
+import { DataTable as CoverletterTable } from "./coverletter/data-table";
+import { columns as CoverletterColumns} from "./coverletter/columns"
+
+export default async function Home() {
+    const applicationDatas = await getApplications();
+    const coverletterDatas = await getCoverletter();
+
+    return (
+        <div className="py-10 mb-25 flex flex-col gap-8">
+            <div className="w-full flex gap-4">
+                <div className="w-1/2">
+                    <ChartPieLabel />
+                </div>
+                <div data-slot="card" className="w-1/2 bg-card text-card-foreground gap-6 rounded-xl border py-6 shadow-sm flex flex-col">
+                    나의 강점
+                </div>
+            </div>
+        
+            <div className="w-full container mx-auto">
+                <ApplicationTable columns={ApplicationColumns} data={applicationDatas} pageSize={3} />
+            </div>
+
+            <div className="w-full container mx-auto">
+                <CoverletterTable columns={CoverletterColumns} data={coverletterDatas} pageSize={3} />
+            </div>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,6 @@
-import { ChartPieLabel } from "@/components/layout/PieChart"; 
-import { getApplications } from "@/service/applications";
-import { DataTable as ApplicationTable } from "./applications/data-table";
-import { columns as ApplicationColumns } from "./applications/columns"
-import { getCoverletter } from "@/service/coverletter";
-import { DataTable as CoverletterTable } from "./coverletter/data-table";
-import { columns as CoverletterColumns} from "./coverletter/columns"
+// src/app/page.tsx
+import { redirect } from "next/navigation";
 
-export default async function Home() {
-    const applicationDatas = await getApplications();
-    const coverletterDatas = await getCoverletter();
-
-    return (
-        <div className="py-10 mb-25 flex flex-col gap-8">
-            <ChartPieLabel />
-            
-            <div className="w-full container mx-auto">
-                <ApplicationTable columns={ApplicationColumns} data={applicationDatas} pageSize={3} />
-            </div>
-
-            <div className="w-full container mx-auto">
-                <CoverletterTable columns={CoverletterColumns} data={coverletterDatas} pageSize={3} />
-            </div>
-        </div>
-    );
+export default function Main() {
+  redirect("/app"); // "/" 요청 들어오면 "/app"으로 보냄
 }
