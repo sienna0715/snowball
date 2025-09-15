92adc9cd69757a1458fbe0cf6519eab989da2b15
Sion Lee <91577106+sienna0715@users.noreply.github.com>
2025-09-15 14:43:53 +0900

    🐛 fix: comment out unused ChartPieLabel

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,4 +1,4 @@
-import { ChartPieLabel } from "@/components/layout/PieChart"; 
+// import { ChartPieLabel } from "@/components/layout/PieChart";
 import { getJobs } from "@/service/job";
 import { DataTable as ApplicationTable } from "./job/data-table";
 import { columns as ApplicationColumns } from "./job/columns"
@@ -12,14 +12,14 @@ export default async function Home() {
 
     return (
         <div className="py-10 mb-25 flex flex-col gap-8">
-            <div className="w-full flex gap-4">
+            {/* <div className="w-full flex gap-4">
                 <div className="w-1/2">
                     <ChartPieLabel />
                 </div>
                 <div data-slot="card" className="w-1/2 bg-card text-card-foreground gap-6 rounded-xl border py-6 shadow-sm flex flex-col">
                     나의 강점
                 </div>
-            </div>
+            </div> */}
         
             <div className="w-full container mx-auto">
                 <ApplicationTable columns={ApplicationColumns} data={applicationDatas} pageSize={3} />
